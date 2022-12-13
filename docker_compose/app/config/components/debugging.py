b21638f526811c1b42fe88b528ccf93b4a1da10f
BuJIKuH <108240404+BuJIKuH@users.noreply.github.com>
2022-12-13 16:38:50 +0300

    Delete debugging.py

--- docker_compose/app/config/components/debugging.py
+++
@@ -1,9 +0,0 @@
-from config.components import installed_apps, middleware
-from config.settings import DEBUG
-
-
-if DEBUG:
-    installed_apps.INSTALLED_APPS += ['debug_toolbar']
-    middleware.MIDDLEWARE += [
-        'debug_toolbar.middleware.DebugToolbarMiddleware',
-    ]
